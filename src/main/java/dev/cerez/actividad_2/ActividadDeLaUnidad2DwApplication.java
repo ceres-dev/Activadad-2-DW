8409fd362daf34b2fe73d61548e4a4be36dabc70
Ceres <cagutierrezpayares@gmail.com>
2026-05-25 13:20:52 -0500

    init

---
+++ src/main/java/dev/cerez/actividad_2/ActividadDeLaUnidad2DwApplication.java
@@ -0,0 +1,13 @@
+package dev.cerez.actividad_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadDeLaUnidad2DwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActividadDeLaUnidad2DwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cerez/actividad_2/ActividadDeLaUnidad2DwApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cerez.actividad_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadDeLaUnidad2DwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
