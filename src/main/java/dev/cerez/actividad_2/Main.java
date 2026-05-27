21cd7187c9b2472f4ac7d28af51041a6e2624d17
Ceres <cagutierrezpayares@gmail.com>
2026-05-27 00:26:29 -0500

    implementar CURD

--- src/main/java/dev/cerez/actividad_2/Main.java
+++ src/main/java/dev/cerez/actividad_2/Main.java
@@ -7,20 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 @SpringBootApplication
 public class Main {
 
-    private final List<Row> rows = new ArrayList<>();
+    private final Map<Integer, Row> rows = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -28,18 +23,133 @@ public class Main {
 
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("rows", rows);
         return "index";
     }
 
-    @PutMapping("/add")
-    public void add() {
+    @PostMapping("/add")
+    public String add(
+            @RequestParam String name,
+            @RequestParam String supplier,
+            @RequestParam String category,
+            @RequestParam String primaryLanguage,
+            @RequestParam String secondaryLanguage,
+            @RequestParam(required = false, defaultValue = "false") Boolean useDB,
+            @RequestParam(required = false, defaultValue = "false") Boolean requiresNetwork,
+            @RequestParam Integer numberOfBits,
+            @RequestParam String license,
+            @RequestParam Double price,
+            @RequestParam String description,
+            @RequestParam Integer website,
+            @RequestParam String email,
+            @RequestParam Long installerSize
+    ) {
+        Row row = createRow(
+                name,
+                supplier,
+                category,
+                primaryLanguage,
+                secondaryLanguage,
+                useDB,
+                requiresNetwork,
+                numberOfBits,
+                license,
+                price,
+                description,
+                website,
+                email,
+                installerSize
+        );
+        rows.put(row.hashCode(), row);
 
+        return "redirect:/";
     }
 
     @PostMapping("/remove")
-    public void remove() {
+    public String remove(@RequestParam Integer key) {
+        rows.remove(key);
 
+        return "redirect:/";
     }
 
+    @GetMapping("/get")
+    @ResponseBody
+    public ResponseEntity<Map<Integer, Row>> get() {
+        return ResponseEntity.ok(rows);
+    }
+
+    @PostMapping("/update")
+    public String update(
+            @RequestParam Integer key,
+            @RequestParam String name,
+            @RequestParam String supplier,
+            @RequestParam String category,
+            @RequestParam String primaryLanguage,
+            @RequestParam String secondaryLanguage,
+            @RequestParam(required = false, defaultValue = "false") Boolean useDB,
+            @RequestParam(required = false, defaultValue = "false") Boolean requiresNetwork,
+            @RequestParam Integer numberOfBits,
+            @RequestParam String license,
+            @RequestParam Double price,
+            @RequestParam String description,
+            @RequestParam Integer website,
+            @RequestParam String email,
+            @RequestParam Long installerSize
+    ) {
+        Row row = createRow(
+                name,
+                supplier,
+                category,
+                primaryLanguage,
+                secondaryLanguage,
+                useDB,
+                requiresNetwork,
+                numberOfBits,
+                license,
+                price,
+                description,
+                website,
+                email,
+                installerSize
+        );
+        rows.remove(key);
+        rows.put(row.hashCode(), row);
+
+        return "redirect:/";
+    }
+
+    private Row createRow(
+            String name,
+            String supplier,
+            String category,
+            String primaryLanguage,
+            String secondaryLanguage,
+            Boolean useDB,
+            Boolean requiresNetwork,
+            Integer numberOfBits,
+            String license,
+            Double price,
+            String description,
+            Integer website,
+            String email,
+            Long installerSize
+    ) {
+        return new Row(
+                name,
+                supplier,
+                category,
+                primaryLanguage,
+                secondaryLanguage,
+                useDB,
+                requiresNetwork,
+                numberOfBits,
+                license,
+                price,
+                description,
+                website,
+                email,
+                installerSize
+        );
+    }
 }
